15ddd8f26b2c2cbb13134cb795e67d562408e165
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-16 13:14:49 +0300

    updated modules and using ejs templates

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
 
-const app = express();
+const app = express()
 let newItems = ["Buy Food", "Cook Food", "Eat Food"]
+let workItems = []
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -11,30 +13,44 @@ app.use(express.static("public"))
 
 //GET Home route
 app.get("/", function(req, res){
-  let today = new Date()
+  
+  let day = date.getDate()
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
-
-  let day = today.toLocaleDateString("en-US", options)
-
-  res.render("list", {kindOfDay: day, newListItems: newItems})
+  res.render("list", {listOfTitle: day, newListItems: newItems})
 });
 
 //POST After clicking button
 app.post("/", function (req,res) {
 
   let newItem = req.body.todo
-  
-  newItems.push(newItem)
 
-  res.redirect("/")
+  if(req.body.list === "Work"){
+    workItems.push(newItem)
+    res.redirect("/work")
+  } else{
+    
+    newItems.push(newItem)
+    res.redirect("/")
+  }
+  })
+
+  app.get("/work", function(req,res){
+
+    res.render("list", {listOfTitle: "Work List", newListItems: workItems})
+  })
+
+  app.post("/work", function (req,res) {
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect("/work")
+    })
+
+app.get("/about", function (req,res) {
+
+  res.render("about")
   })
 
 
 app.listen(3000, function(){
-  console.log("Server started on port 3000.");
-});
+  console.log("Server started on port 3000.")
+})
