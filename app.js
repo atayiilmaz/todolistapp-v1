dbb580cea85b65fc04f1921be65ee6cce73e625e
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-18 21:39:55 +0300

    added mongoose

--- app.js
+++ app.js
@@ -2,14 +2,39 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose")
 
 const app = express()
-let newItems = ["Buy Food", "Cook Food", "Eat Food"]
-let workItems = []
+/*let newItems = ["Buy Food", "Cook Food", "Eat Food"]
+let workItems = []*/
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
+
+//Database connection
+mongoose.connect("mongodb://0.0.0.0:27017/todolistDB", {useNewUrlParser: true})
+
+const Schema = mongoose.Schema
+const itemSchema = new Schema({name: String})
+
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+})
+
+const defaultItem = [item1,item2,item3]
+
+//Item.insertMany(defaultItem)
 
 //GET Home route
 app.get("/", function(req, res){
